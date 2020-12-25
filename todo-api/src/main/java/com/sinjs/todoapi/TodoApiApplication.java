bf850a736ebfb170ba1ec69b2bc42e3c0146624e
Jack <jack.niu@outlook.com>
2020-12-25 08:33:23 +0800

    add missing imports

--- todo-api/src/main/java/com/sinjs/todoapi/TodoApiApplication.java
+++ todo-api/src/main/java/com/sinjs/todoapi/TodoApiApplication.java
@@ -2,27 +2,31 @@ package com.sinjs.todoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @EnableOpenApi
 @SpringBootApplication
 public class TodoApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApiApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("GET","PUT","POST","OPTIONS","DELETE")
-						.allowCredentials(false).maxAge(3600);
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "OPTIONS", "DELETE")
+                        .allowCredentials(false).maxAge(3600);
+            }
+        };
+    }
 
 }
